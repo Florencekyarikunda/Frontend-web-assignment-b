1c46a849c7775d51b9f291ca143c8c5a82c44c09
Florencekyarikunda <79209854+Florencekyarikunda@users.noreply.github.com>
2021-03-11 09:16:54 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,8 @@
+function fruits() {
+    var fruitsList=["mangoes","oranges","avacados","kiwi"];
+    for (let i = 0; i <= fruitsList.length; i++) {
+        const element = fruitsList[i];
+        console.log(fruitsList[i]);
+    }
+}
+fruits();
